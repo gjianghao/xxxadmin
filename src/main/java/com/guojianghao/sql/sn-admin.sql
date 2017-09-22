cdf9665be234f4450cd5cf8c7afa1cbb1a91b92e
gjianghao <825304840@qq.com>
2017-09-22 17:44:07 +0800

    xxxadmin

--- src/main/java/com/guojianghao/sql/sn-admin.sql
+++ src/main/java/com/guojianghao/sql/sn-admin.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2017-09-20 18:02:48
+Date: 2017-09-22 17:42:54
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -26,16 +26,17 @@ CREATE TABLE `sys_resource` (
   `href` varchar(100) DEFAULT NULL,
   `create_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   `status` char(1) NOT NULL DEFAULT '0',
+  `description` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of sys_resource
 -- ----------------------------
-INSERT INTO `sys_resource` VALUES ('1', '系统管理', null, null, '2017-09-20 10:19:53', '0');
-INSERT INTO `sys_resource` VALUES ('2', '用户管理', '1', 'sysUser/toView', '2017-09-20 15:53:31', '0');
-INSERT INTO `sys_resource` VALUES ('3', '角色管理', '1', 'sysRole/toView', '2017-09-20 15:53:33', '0');
-INSERT INTO `sys_resource` VALUES ('4', '资源管理', '1', 'sysResource/toView', '2017-09-20 15:53:36', '0');
+INSERT INTO `sys_resource` VALUES ('1', '系统管理', null, null, '2017-09-21 14:20:01', '0', '超级管理员拥有系统管理');
+INSERT INTO `sys_resource` VALUES ('2', '用户管理', '1', 'sysUser/toView', '2017-09-21 14:20:09', '0', '用来管理用户');
+INSERT INTO `sys_resource` VALUES ('3', '角色管理', '1', 'sysRole/toView', '2017-09-21 14:20:18', '0', '用来管理角色');
+INSERT INTO `sys_resource` VALUES ('4', '资源管理', '1', 'sysResource/toView', '2017-09-21 18:32:44', '0', '用来管理访问的资源xxx');
 
 -- ----------------------------
 -- Table structure for `sys_role`
@@ -45,13 +46,17 @@ CREATE TABLE `sys_role` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   `status` char(1) NOT NULL DEFAULT '0',
+  `description` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of sys_role
 -- ----------------------------
-INSERT INTO `sys_role` VALUES ('1', 'admin', '0');
+INSERT INTO `sys_role` VALUES ('1', 'admin', '0', '一切权利都在我的手中');
+INSERT INTO `sys_role` VALUES ('2', '商务', '0', '我是小小的商务啊');
+INSERT INTO `sys_role` VALUES ('3', '运营', '0', '我是小小的运营');
+INSERT INTO `sys_role` VALUES ('6', '技术', '0', '负责技术开发');
 
 -- ----------------------------
 -- Table structure for `sys_role_resource`
@@ -62,15 +67,20 @@ CREATE TABLE `sys_role_resource` (
   `role_id` int(10) DEFAULT NULL,
   `resource_id` int(10) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of sys_role_resource
 -- ----------------------------
-INSERT INTO `sys_role_resource` VALUES ('1', '1', '1');
-INSERT INTO `sys_role_resource` VALUES ('2', '1', '2');
-INSERT INTO `sys_role_resource` VALUES ('3', '1', '3');
-INSERT INTO `sys_role_resource` VALUES ('4', '1', '4');
+INSERT INTO `sys_role_resource` VALUES ('51', '2', '1');
+INSERT INTO `sys_role_resource` VALUES ('52', '2', '3');
+INSERT INTO `sys_role_resource` VALUES ('53', '2', '4');
+INSERT INTO `sys_role_resource` VALUES ('54', '1', '1');
+INSERT INTO `sys_role_resource` VALUES ('55', '1', '2');
+INSERT INTO `sys_role_resource` VALUES ('56', '1', '3');
+INSERT INTO `sys_role_resource` VALUES ('57', '1', '4');
+INSERT INTO `sys_role_resource` VALUES ('59', '6', '1');
+INSERT INTO `sys_role_resource` VALUES ('60', '6', '4');
 
 -- ----------------------------
 -- Table structure for `sys_user`
@@ -83,12 +93,15 @@ CREATE TABLE `sys_user` (
   `password` varchar(32) DEFAULT NULL,
   `create_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
 INSERT INTO `sys_user` VALUES ('1', 'admin', 'admin', '20d8f66e87a46e21de78ea4efd661069', '2017-09-20 09:58:44');
+INSERT INTO `sys_user` VALUES ('14', 'king', 'guojianghao', '20d8f66e87a46e21de78ea4efd661069', '2017-09-22 17:08:20');
+INSERT INTO `sys_user` VALUES ('15', '张聪明', 'zhangcongming', '20d8f66e87a46e21de78ea4efd661069', '2017-09-22 17:08:41');
+INSERT INTO `sys_user` VALUES ('16', '姚志祥', 'yaozhixiang', '20d8f66e87a46e21de78ea4efd661069', '2017-09-22 17:08:59');
 
 -- ----------------------------
 -- Table structure for `sys_user_role`
@@ -99,9 +112,12 @@ CREATE TABLE `sys_user_role` (
   `user_id` int(10) DEFAULT NULL,
   `role_id` int(10) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of sys_user_role
 -- ----------------------------
-INSERT INTO `sys_user_role` VALUES ('1', '1', '1');
+INSERT INTO `sys_user_role` VALUES ('11', '1', '1');
+INSERT INTO `sys_user_role` VALUES ('13', '14', '6');
+INSERT INTO `sys_user_role` VALUES ('14', '15', '6');
+INSERT INTO `sys_user_role` VALUES ('15', '16', '6');
